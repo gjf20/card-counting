ac50048582016f0a4e1d8ba6b3eddb20e21a6f13
Jamie Flynn <gjf20@case.edu>
2021-12-05 21:17:12 -0700

    Deck has all standard cards and can generate multiple copies

--- app/src/main/java/com/example/myfirstapp/data_model/Deck.kt
+++ app/src/main/java/com/example/myfirstapp/data_model/Deck.kt
@@ -1,4 +1,22 @@
 package com.example.myfirstapp.data_model
 
 class Deck {
+    private val standardDeckSize = 52
+    private val cardArr : Array<Card>
+    private var topCardIndex = 0
+
+    constructor(numDecks : Int) {
+        cardArr = Array<Card>(numDecks * standardDeckSize, fun(i : Int): Card {return Card(i+1)})
+    }
+
+    fun getLength() : Int {
+        return cardArr.size
+    }
+
+    fun drawCard() : Card {
+        val card = cardArr[topCardIndex]
+        topCardIndex++
+        return card
+    }
+
 }
