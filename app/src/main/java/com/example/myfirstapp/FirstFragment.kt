378eb7c46077c4f591b5d373cb50a39eb299bfa2
Jamie Flynn <gjf20@case.edu>
2021-12-11 20:07:10 -0700

    Draw next card puts the card in the correct place

--- app/src/main/java/com/example/myfirstapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.kt
@@ -10,6 +10,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.myfirstapp.data_model.Deck
 import com.example.myfirstapp.databinding.FragmentFirstBinding
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 /**
@@ -49,6 +50,7 @@ class FirstFragment : Fragment() {
     }
 
     private fun showNextCard() {
+
         val card = deck.drawCard()
 
         //create the image view with the card's image
@@ -60,9 +62,26 @@ class FirstFragment : Fragment() {
         imageView.setImageResource(res)
 
         // insert into main fragment view
-        binding.firstFragConstraintLayout.addView(imageView,100, 100)
+        val layoutParams = getLocationInFragment()
+        binding.firstFragConstraintLayout.addView(imageView, layoutParams)
         imageView.bringToFront()
         imageView.setBackgroundColor(Color.MAGENTA)
     }
 
+    private fun getPixels(dps: Int): Int { //TODO move this to common
+        val scale = requireContext().resources.displayMetrics.density
+        return (dps * scale + 0.5f).toInt()
+    }
+
+    private fun getLocationInFragment() : ViewGroup.LayoutParams {
+        val viewWidth = 148
+        val viewHeight = 215
+
+        val layoutParams = ConstraintLayout.LayoutParams(getPixels(viewWidth), getPixels(viewHeight))
+        layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+        layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID
+        layoutParams.bottomToTop = binding.buttonNextCard.id
+        return layoutParams
+    }
 }
